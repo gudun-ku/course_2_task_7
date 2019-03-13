634c4edac674273b30d096c70994d56b4b7fa906
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2019-03-13 17:53:39 +0300

    Database creation options changed

--- app/src/main/java/elegion/com/roomdatabase/AppDelegate.java
+++ app/src/main/java/elegion/com/roomdatabase/AppDelegate.java
@@ -17,6 +17,7 @@ public class AppDelegate extends Application {
     public void onCreate() {
         super.onCreate();
         mMusicDatabase = Room.databaseBuilder(getApplicationContext(), MusicDatabase.class, "music_database")
+                .fallbackToDestructiveMigration()
                 .allowMainThreadQueries()
                 .build();
     }
--- app/src/main/java/elegion/com/roomdatabase/database/MusicDatabase.java
+++ app/src/main/java/elegion/com/roomdatabase/database/MusicDatabase.java
@@ -7,7 +7,7 @@ import android.arch.persistence.room.RoomDatabase;
  * @author Azret Magometov
  */
 
-@Database(entities = {Album.class, Song.class, AlbumSong.class}, version = 1)
+@Database(entities = {Album.class, Song.class, AlbumSong.class}, version = 2)
 public abstract class MusicDatabase extends RoomDatabase {
     public abstract MusicDao getMusicDao();
 
